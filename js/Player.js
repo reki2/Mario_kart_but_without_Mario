294c89b33e7d049c6f730c47a377b365fec9c601
reki2 <83732645+reki2@users.noreply.github.com>
2021-08-02 16:47:21 -0500

    :)

--- js/Player.js
+++ js/Player.js
@@ -2,14 +2,13 @@ class Player {
     constructor(){
     this.index = null;
     this.name = null;
-    this.distance = 0
+    this.distance = 0;
     }
  getCount(){
      var playerCount_RO = database.ref('playerCount');
-     playerCount_RO.on('value', function(data){
+     playerCountRef.on("value",(data)=>{
         playerCount = data.val();
-     })
- //console.log('soda')
+     });
     }
 updateCount(count){
 database.ref('/').update({
@@ -18,11 +17,14 @@ playerCount: count
 }
 
 update(){
-var playerIndex = 'players/player' +this.index;
-database.ref(playerIndex).set({name: this.name, distance : this.distance})
+var playerIndex = "players/player" + this.index;
+database.ref(playerIndex).set({
+name:this.name,
+distance:this.distance 
+}); 
 }
 static getPlayerInfo(){
  var playerinfoRef = database.ref('players');
  playerinfoRef.on('value', (data)=>{ allplayers = data.val(); })
  }
-}    
+}
